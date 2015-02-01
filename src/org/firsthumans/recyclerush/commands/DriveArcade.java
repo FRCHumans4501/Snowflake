7f5329f6e1425c9f06315803ee7bcf1d825b4eef
Jerish Brown <thejerishbrown@gmail.com>
2015-01-31 17:36:22 -0800

    Started DriveTrain code, needs testing.

---
+++ src/org/firsthumans/recyclerush/commands/DriveArcade.java
@@ -0,0 +1,49 @@
+package org.firsthumans.recyclerush.commands;
+
+import org.firsthumans.recyclerush.OI;
+import org.firsthumans.recyclerush.Robot;
+import org.firsthumans.recyclerush.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveArcade extends Command {
+
+	DriveTrain driveTrain;
+	OI oi;
+	
+    public DriveArcade() {
+        requires(Robot.driveTrain);
+        
+        driveTrain = Robot.driveTrain;
+        oi = Robot.oi;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double strafe = oi.getX();
+    	double forward = oi.getY();
+    	double rotate = oi.getTriggers();
+    	driveTrain.arcadeDrive(strafe, forward, rotate);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/firsthumans/recyclerush/subsystems/DriveTrain.java
@@ -0,0 +1,32 @@
+package org.firsthumans.recyclerush.subsystems;
+
+import org.firsthumans.recyclerush.commands.DriveIdle;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem {
+	RobotDrive drive;
+	Talon strafeWheel;
+	Timer time;
+	
+	public static int X = 1;
+	
+	public DriveTrain(int frontLeftMotor, int frontRightMotor, int rearLeftMotor, int rearRightMotor, int strafeMotor) {
+		drive = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+		strafeWheel = new Talon(strafeMotor);
+	}
+
+    public void initDefaultCommand() {
+        setDefaultCommand(new DriveIdle());
+    }
+       
+    public void arcadeDrive(double forward, double strafe, double rotate) {
+    	// NOTE(jerish): Implement gyro / encoder drive correction
+    	drive.arcadeDrive(forward, rotate);
+    	strafeWheel.set(strafe);
+    }
+}
+
